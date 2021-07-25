dde92651505dfc2d40255aa40949d52851f620d2
river-lat <lc93fnv4@gmail.com>
2021-07-25 17:58:59 +0900

    kyougou syuusei2

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -10,7 +10,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
     Monster monster = new Monster();
-<<<<<<< HEAD
+
     // Player player = new Player("test player", 100, 7);
     SeMonsterGame SeMonsterGame = new SeMonsterGame(createRandomList(5));
     // player.drawMonsters();
@@ -25,12 +25,5 @@ public class App {
   public static int createRandomList(int count) {
     Random random = new Random();
     return (int) random.nextInt(count + 1);
-=======
-    Player player = new Player("test player", 100, 7);
-    // player.drawMonsters();
-
-    System.out.println(monster.toString());
-    System.out.println(player.toString());
->>>>>>> main
   }
 }
